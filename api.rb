a93854129bce00d29107918b8478b12ec59ddb33
Mattt Thompson <m@mattt.me>
2012-02-16 15:34:44 -0600

    Adding missing config.ru
    Wrapping implicit Sinatra app in class

--- api.rb
+++ api.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'json'
 
-get '/sushi.json' do
-  content_type :json
-  return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+class API < Sinatra::Base
+  get '/sushi.json' do
+    content_type :json
+    
+    {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+  end
 end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './api'
+
+run API
